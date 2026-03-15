c6e980d0f43fe56e445e52c632bc2e1ef3fc39b8
ROSHAN <roshanprajapati961@gmail.com>
2026-03-16 02:31:41 +0530

    API

--- index.js
+++ index.js
@@ -265,6 +265,5 @@ app.post('/api/attendance/mark', verifyToken, async (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Master API Server running on port ${PORT}`);
-    console.log(`Local network par connect karne ke liye IP check karein!`);
+    console.log(`Server is running on port ${PORT}`);
 });
